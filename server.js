744b27c813f048c0ec7f48b3f4e85f1326e6ac92
mariananevesz <nevespereiramariana9@gmail.com>
2026-09-10 13:46:52 -0300

    Commit 3

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const __dirname = pathAbsolute.slice(1);
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'assets')))
+
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'views', 'home.html');
     res.sendFile(filePath);
