5bdd5f33efe10cf31ee1119d8d33c12c09553b6a
mariananevesz <nevespereiramariana9@gmail.com>
2026-09-10 14:21:25 -0300

    Commit 5

--- server.js
+++ server.js
@@ -40,6 +40,45 @@ app.get('/recuperar-senha', (req, res) => {
     res.sendFile(filePath);
 })
 
+app.post('/clientes', (req, res) => {
+    const { nome, sobrenome, cpf, dataNascimento, telefone, email, senha } = req.body;
+
+    res.send(`<h1>Dados recebidos</h1>
+        <p>Nome: ${nome}</p>
+        <p>Sobrenome: ${sobrenome}</p>
+        <p>CPF: ${cpf}</p>
+        <p>Data de Nascimento: ${dataNascimento}</p>
+        <p>Telefone: ${telefone}</p>
+        <p>Email: ${email}</p>
+        <p>Senha: ${senha}</p>`)
+})
+
+app.post('/funcionarios', (req, res) => {
+    const {inputNomeFunc,inputSobrenomeFunc,inputCPFFunc,inputDataNascFunc,inputTelefoneFunc,inputCargoFunc,inputSalarioFunc,inputEmailFunc,inputSenha } = req.body;
+
+    res.send(`<h1>Dados recebidos</h1>
+        <p>Nome: ${inputNomeFunc}</p>
+        <p>Sobrenome: ${inputSobrenomeFunc}</p>
+        <p>CPF: ${inputCPFFunc}</p>
+        <p>Data de Nascimento: ${inputDataNascFunc}</p>
+        <p>Telefone: ${inputTelefoneFunc}</p>
+        <p>Cargo: ${inputCargoFunc}</p>
+        <p>Salário: ${inputSalarioFunc}</p>
+        <p>Email: ${inputEmailFunc}</p>
+        <p>Senha: ${inputSenha}</p>`)
+})
+
+app.post('/produtos', (req, res) => {
+    const {inputNomeProd,inputFabricanteProd,inputDescricaoProd,inputValorProd,inputQtdProd } = req.body;
+
+    res.send(`<h1>Dados recebidos</h1>
+        <p>Nome: ${inputNomeProd}</p>
+        <p>Fabricante: ${inputFabricanteProd}</p>
+        <p>Descrição: ${inputDescricaoProd}</p>
+        <p>Valor: ${inputValorProd}</p>
+        <p>Quantidade: ${inputQtdProd}</p>`)
+})
+
 app.listen(port, () => {
     console.log(`Servidor ativo rodando na porta ${port}`);
 })
